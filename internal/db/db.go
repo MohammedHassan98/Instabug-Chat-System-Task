0dd6900fdb37acdf58c3db5e0d5f2b981633741c
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2024-11-12 00:16:43 +0200

    feat: switch to using grom as ORM for DB

--- internal/db/db.go
+++ internal/db/db.go
@@ -8,6 +8,8 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 var DB *sql.DB
@@ -18,15 +20,22 @@ func Connect() {
 	}
 
 	dbURL := os.Getenv("DATABASE_URL")
-	var err error
-	DB, err = sql.Open("mysql", dbURL)
+	dsn := dbURL + "?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("Could not connect to database: %v", err)
 	}
 
-	if err = DB.Ping(); err != nil {
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalf("Could not get underlying *sql.DB: %v", err)
+	}
+
+	if err = sqlDB.Ping(); err != nil {
 		log.Fatalf("Could not ping the database: %v", err)
 	}
 
+	DB = sqlDB
 	log.Println("Connected to database")
 }
