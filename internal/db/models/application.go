890fe645ce61e6f3a369ccfdd2bf865e7009b96a
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2024-11-12 19:19:35 +0200

    feat: add some enhancements to the models & add relations

--- internal/db/models/application.go
+++ internal/db/models/application.go
@@ -7,10 +7,11 @@ import (
 )
 
 type Application struct {
-	ID    uint   `gorm:"primarykey"`
-	Name  string `gorm:"size:255;not null"`
-	Token string `gorm:"size:255;not null;unique"`
-	Chats []Chat `gorm:"foreignKey:ApplicationID"`
+	ID         uint   `gorm:"primarykey"`
+	Name       string `gorm:"size:255;not null"`
+	Token      string `gorm:"size:255;not null;unique;index"`
+	ChatsCount int    `gorm:"default:0"`
+	Chats      []Chat `gorm:"foreignKey:ApplicationID"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
--- internal/db/models/message.go
+++ internal/db/models/message.go
@@ -6,11 +6,14 @@ import (
 
 type Message struct {
 	ID            uint   `gorm:"primaryKey"`
-	ChatID        uint   `gorm:"not null;index"` // Foreign key referencing Chat
-	MessageNumber int    `gorm:"not null;index"`
+	ChatID        uint   `gorm:"not null;index"`
+	MessageNumber int    `gorm:"not null"`
 	Body          string `gorm:"type:text;not null"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
 
 	Chat Chat `gorm:"constraint:OnDelete:CASCADE"`
+
+	// Add unique composite index for ChatID and MessageNumber
+	Index []string `gorm:"uniqueIndex:idx_chat_message_number"`
 }
