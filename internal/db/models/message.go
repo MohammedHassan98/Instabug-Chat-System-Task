db9f079982835d5702c2b1ed8b35322db298f819
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2024-11-14 19:23:46 +0200

    fix: update message number index type

--- internal/db/models/message.go
+++ internal/db/models/message.go
@@ -7,7 +7,7 @@ import (
 type Message struct {
 	ID            uint   `gorm:"primaryKey"`
 	ChatID        uint   `gorm:"not null;index"`
-	MessageNumber int    `gorm:"not null;uniqueIndex:idx_chat_message_number"`
+	MessageNumber int    `gorm:"not null;index:idx_chat_message_number"`
 	Body          string `gorm:"type:text;not null"`
 	CreatedAt     time.Time
 	UpdatedAt     time.Time
