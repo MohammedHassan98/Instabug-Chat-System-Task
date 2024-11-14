1a92ddda4411961c2837ff63a42f0a9a2aa0aa9a
MohammedHassan98 <mohammedhassanedofficial@gmail.com>
2024-11-14 19:13:07 +0200

    fix: handle fixing chat number index type

--- internal/db/models/chat.go
+++ internal/db/models/chat.go
@@ -7,7 +7,7 @@ import (
 type Chat struct {
 	ID            uint      `gorm:"primaryKey"`
 	ApplicationID uint      `gorm:"not null;index"`
-	ChatNumber    int       `gorm:"not null;uniqueIndex:idx_application_chat_number"`
+	ChatNumber    int       `gorm:"not null;index:idx_application_chat_number"`
 	MessagesCount int       `gorm:"default:0"`
 	Messages      []Message `gorm:"foreignKey:ChatID"`
 	CreatedAt     time.Time
